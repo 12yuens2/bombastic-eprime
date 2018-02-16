3a8a767c2b64b69a5cc3e63afca411c914fa995f
12yuens2 <sizhe1007@gmail.com>
2018-02-16 17:55:14 +0000

    Start on script to run experiments.

---
+++ script.py
@@ -0,0 +1,10 @@
+import subprocess
+
+# Run all params
+for param in "params":
+    subprocess.run(["../savilerow", "Bombastic.eprime", param])
+
+# Read info files to get time taken
+
+
+# Save data to file
